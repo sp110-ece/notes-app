6f044377c4c78be022af66fd2744275cace648bf
Siddarth Pattisapu <sp110@illinois.edu>
2025-05-23 11:13:18 -0500

    notely

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -38,6 +38,8 @@ const Dashboard = () => {
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
   const [newDisabled, setDisabled] = useState(false)
+  const [isSummaryLoading, setSummaryLoading] = useState(false)
+  const [isLinkLoading, setLinkLoading] = useState(false)
 
 
   useEffect(() => {
@@ -127,6 +129,8 @@ const Dashboard = () => {
       // throw new Error('Input too long to summarize');
       setError("The note is too long to summarize");
     }
+    setSummaryLoading(true)
+    setLinkLoading(true)
     const res = await fetch('/api/summarize', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -138,12 +142,14 @@ const Dashboard = () => {
     }
 
     const data = await res.json();
+    setSummaryLoading(false)
     setSummary(data.summary);
     getResources(data.summary)
     return data;
   }
 
   async function getResources(summary: string) {
+    
     const res = await fetch('/api/search', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -155,6 +161,7 @@ const Dashboard = () => {
     }
 
     const resources = await res.json();
+    setLinkLoading(false)
     setResources({ items: resources });
     //console.log(resources);
     return resources;
@@ -205,12 +212,7 @@ const Dashboard = () => {
     const updatedData = await updatedNotes.json();
     setNotes(updatedData);
 
-    setDisabled(true)
-    const timer = setTimeout(() => {
-      setDisabled(false);
-      console.log(newDisabled)
-    }, 3000);
-    return() => clearTimeout(timer);
+   
     
 
 
@@ -238,13 +240,11 @@ const Dashboard = () => {
           
 
         }} 
-        
       />
       <main className="flex flex-col items-center justify-start flex-1 p-10 space-y-8 min-h-screen">
         <div className="absolute top-5 right-5">
           <SignOut />
         </div>
-
         <h1 className="text-5xl font-extrabold select-none">
           What&apos;s on your mind...
         </h1>
@@ -252,13 +252,12 @@ const Dashboard = () => {
             <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', width: "0", flexGrow: 1, overflowY: 'auto', height: '75vh'}}>
                 <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap' }}>
                   <button onClick={handleSubmit} style={{resize: 'none' }}>Save Note</button>
-                  <button onClick={handleNewNote} disabled={newDisabled}>New Note</button>
+                  <button onClick={handleNewNote} disabled={isLinkLoading}>New Note</button>
                   <div style={{ minWidth: '200px', color: 'red', fontSize: '1rem' }}>
                     <span style={{color: 'red'}}> {error || <span>&nbsp;</span>}</span>
                     <span style={{color: 'green'}}> {success || <span>&nbsp;</span>}</span>
                   </div>
                   <div style={{ minWidth: '200px', color: 'green', fontSize: '1rem' }}>
-                    
                   </div>
                 </div>
                 <input
@@ -271,7 +270,6 @@ const Dashboard = () => {
                     ...(error && desc === '' ? {borderColor: 'red'} : {}) 
                   }}
                   className={'border border-red-500'}
-                  
                 />
                 <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
                   <textarea
@@ -286,24 +284,37 @@ const Dashboard = () => {
                     className={`w-full ${error && desc === '' ? 'border border-red-500' : ''}`}
                   />   
                 </div>
-              
             </div>
             <div style={{ padding: '1rem', flex: 1, height: '75vh'}}>
               <div style={{ padding: '1rem', minHeight: 0, borderRadius: '8px', background: 'var(--color-accent-light)', color: 'var(--color-foreground)', marginBottom: '1rem', flex: 1, height: '50%' }}>
                 <h2 style={{ fontSize: '1.25rem', fontWeight: '100', marginBottom: '0.5rem' }}>Summary</h2>
-                <p style={{ color: 'var(--color-foreground)' }}>{summary || ''}</p>
+                {isSummaryLoading ? (
+                  <div className="flex items-center justify-center h-1/2">
+                    <div className="animate-spin h-8 w-8 border-4 border-gray-300 border-t-gray-500 rounded-full mx-auto"></div>
+                  </div>
+                  
+                ): (
+                  <p style={{ color: 'var(--color-foreground)' }}>{summary || ''}</p>
+                )
+                }
+                
               </div>
               <div>
               </div>
               <div style={{ resize: 'none', minHeight: 0, padding: '1rem', borderRadius: '8px', background: 'var(--color-accent-light)', color: 'var(--color-foreground)', flex: 1, height: '50%', overflowY: 'auto'}}>
-                <h2 style={{ fontSize: '1.25rem', fontWeight: '100', marginBottom: '0.5rem' }}>Resources</h2>
-                <ul style={{ color: 'var(--color-foreground)' }}>
-                  {resources && resources.items && resources.items.map((item, index) => (
-                    <li key={index}>
-                      {item.title} : <a href={item.url} style={{ wordBreak: 'break-word' }}>{item.url}</a>
-                    </li>
-                  ))}
-                </ul>
+                <h2 style={{ fontSize: '1.25rem', fontWeight: '100', marginBottom: '0.5rem' }}>Related Links</h2>
+                
+                  {isLinkLoading ? (
+                    <div className="flex items-center justify-center h-1/2">
+                      <div className="animate-spin h-8 w-8 border-4 border-gray-300 border-t-gray-500 rounded-full mx-auto"></div>
+                    </div>
+                  ) : (
+                    resources && resources.items && resources.items.map((item, index) => (
+                      <li key={index}>
+                        {item.title} : <a href={item.url} style={{ wordBreak: 'break-word' }}>{item.url}</a>
+                      </li>
+                    ))
+                  )} 
               </div>
             </div>
           </div>
